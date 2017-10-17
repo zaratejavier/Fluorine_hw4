74c29043f13e85f401c809d0320bb7988a070986
AndrewBills <31480930+AndrewBills@users.noreply.github.com>
2017-10-16 22:06:04 -0600

    getting very very close to final version
    getting very very close to final version

--- fluorine_hw4_task1.cpp
+++ fluorine_hw4_task1.cpp
@@ -5,7 +5,18 @@
 #include <string>
 #include <iomanip>
 #include <algorithm>
+#include <sstream>
+
 using namespace std;
+
+//this template is lets me make the command to_string which allows me to convert a integer to a string
+template <typename T>
+string to_string(T Number)
+{
+    ostringstream OutputStringStream;
+    OutputStringStream << Number;
+    return OutputStringStream.str();
+}
 
 class HouseInfo{ 
 	private:
@@ -16,24 +27,23 @@ class HouseInfo{
 	public:
 		HouseInfo() : streetNumber(0),streetName(""), Price(0.0) {}
 		
-		//getter///////////////////////////////////
+		//getter//////////////////////////////////////////////
 		int getPrice(){return Price;}
-		
-		
+		//prints out what 3 houses the user typed in//////////
 		void printMarketAnalysis();
 		//returns a string on which house you should purchase
-		string youShouldBuy(){return "House number"+houseOrderEntered;}
-		//aquireing the info from the user////////
+		string youShouldBuy(){return "   House number "+to_string(houseOrderEntered);}
+		//aquireing the info from the user///////////////////
 		void aquirehouseInfo(int i);
-}
-
-void HouseInfo::printMarketAnalysis();
+};
+//prints out what 3 houses the user typed in//////////
+void HouseInfo::printMarketAnalysis()
 {
 			cout << houseOrderEntered << " House " << streetNumber << " " 
 			 << streetName << fixed << setprecision(2) << " [$" << Price << "]" << endl;
 }
 //aquireing the info from the user////////
-void aquirehouseInfo(int i)
+void HouseInfo::aquirehouseInfo(int i)
 {
 	cout << "Enter info for " << i << " house" << endl  << "Please enter the street Number:" << endl;
 	cin >> streetNumber;
@@ -43,9 +53,8 @@ void aquirehouseInfo(int i)
 	cout << "Please enter the Price:" << endl;
 	cin >> Price;
 	cout << endl << endl;
-	houseOrderEntered=i;
+	houseOrderEntered=i;//this help me keep trake of the house order
 }
-
 
 
 //this function compares the prices of each house and prints them out from least to greatest and tell you one is the cheapest
@@ -55,20 +64,23 @@ void comparePrices(HouseInfo h[2]);
 // Main function ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 int main() {
 	HouseInfo house_array[3]; //array of objects
-	for(int i=1; i<4; i++)house_array[(i-1)].aquirehouseInfo(i);
-	comparePrices(house_array);
+	for(int i=1; i<4; i++)house_array[(i-1)].aquirehouseInfo(i);//calls the function that gets house information from the user
+	comparePrices(house_array);// compares the price of each house
 	return 0;
 }
-
 void comparePrices(HouseInfo h[2])
 {
     cout << "Your Market analysis is as follows:" << endl;
     for(int i=0; i<3; i++)
     	h[i].printMarketAnalysis();	
+    	
+    // The code below organizes the objects according to the price of each house from least to greatest
     for(int i=0;i<3;i++)
         for (int j=0; j<3;j++)
-			if(h[i].getPrice()>h[j].getPrice())
+			if(h[j].getPrice()>h[i].getPrice())
 				swap(h[i], h[j]);
+				
+	// The code below prints out the first house that is the cheapest, and the other two if they equal the first house in the object array
     cout << "You should buy:";
     for(int i=0; i<3; i++) 
 		if(h[0].getPrice() == h[i].getPrice()) 
