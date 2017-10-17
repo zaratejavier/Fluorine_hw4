2355ea3fd1675eb72f5c05308bb577ef8b6c288c
AndrewBills <31480930+AndrewBills@users.noreply.github.com>
2017-10-16 21:09:52 -0600

    I'm almost finished with all the touch ups.

--- fluorine_hw4_task1.cpp
+++ fluorine_hw4_task1.cpp
@@ -1,9 +1,5 @@
-// file: Real_SaltLake_HW3_task2.cpp
-// Created by Daryl Adams on 9/26/17 for CS1410. Real SaltLake
-// Copyright (c) 2017 WSU
-
-//
-
+// file: Florine_HW4.cpp
+// Created by Andrew Bills/Javier Zarate on 10/16/2017 for CS1410. Florine
 
 #include <iostream>
 #include <string>
@@ -16,33 +12,46 @@ class HouseInfo{
 		int streetNumber;
 		string streetName;
 		double Price;
+		int houseOrderEntered;
 	public:
 		HouseInfo() : streetNumber(0),streetName(""), Price(0.0) {}
 		
-		//getter/////////////////////////
+		//getter///////////////////////////////////
 		int getPrice(){return Price;}
 		
-		int printMarketAnalysis(int i)
-		{
-			cout << (i+1) << " House " << streetNumber << " " 
-			 << streetName << fixed << setprecision(2) << " [$" << Price << "]" << endl;
-		}
 		
-		
-		void aquirehouseInfo(int i)
-		{
-			cout << "Enter info for " << i << " house" << endl  << "Please enter the street Number:" << endl;
-			cin >> streetNumber;
-			cout << "Please enter the street Name:" << endl;
-			cin.ignore();
-			getline(cin, streetName);
-			cout << "Please enter the Price:" << endl;
-			cin >> Price;
-			cout << endl << endl;
-    		}
-};
+		void printMarketAnalysis();
+		//returns a string on which house you should purchase
+		string youShouldBuy(){return "House number"+houseOrderEntered;}
+		//aquireing the info from the user////////
+		void aquirehouseInfo(int i);
+}
 
+void HouseInfo::printMarketAnalysis();
+{
+			cout << houseOrderEntered << " House " << streetNumber << " " 
+			 << streetName << fixed << setprecision(2) << " [$" << Price << "]" << endl;
+}
+//aquireing the info from the user////////
+void aquirehouseInfo(int i)
+{
+	cout << "Enter info for " << i << " house" << endl  << "Please enter the street Number:" << endl;
+	cin >> streetNumber;
+	cout << "Please enter the street Name:" << endl;
+	cin.ignore();
+	getline(cin, streetName);
+	cout << "Please enter the Price:" << endl;
+	cin >> Price;
+	cout << endl << endl;
+	houseOrderEntered=i;
+}
+
+
+
+//this function compares the prices of each house and prints them out from least to greatest and tell you one is the cheapest
 void comparePrices(HouseInfo h[2]);
+
+
 // Main function ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 int main() {
 	HouseInfo house_array[3]; //array of objects
@@ -53,11 +62,15 @@ int main() {
 
 void comparePrices(HouseInfo h[2])
 {
-	for(int i=0;i<3;i++)
-        for (int j=0; j<3;j++)
-			if(h[i].getPrice()>h[j].getPrice()) 
-				swap(h[i], h[j]);
     cout << "Your Market analysis is as follows:" << endl;
+    for(int i=0; i<3; i++)
+    	h[i].printMarketAnalysis();	
+    for(int i=0;i<3;i++)
+        for (int j=0; j<3;j++)
+			if(h[i].getPrice()>h[j].getPrice())
+				swap(h[i], h[j]);
+    cout << "You should buy:";
     for(int i=0; i<3; i++) 
-    	h[i].printMarketAnalysis(i);	
+		if(h[0].getPrice() == h[i].getPrice()) 
+			cout << h[i].youShouldBuy();
 }
